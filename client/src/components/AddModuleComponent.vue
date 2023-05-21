e642a75cb21ba06a750131b85dfc25a663d1ce21
mti <mti@purdue.edu>
2023-05-20 21:42:58 -0400

    worked on add module

--- client/src/components/AddModuleComponent.vue
+++ client/src/components/AddModuleComponent.vue
@@ -8,27 +8,71 @@
     </div>
     <button
       class="m-5 h-16 bg-white/25 w-32 rounded-lg text-white font-semibold shadow-xl shadow-indigo-400/25 content-center p-2"
-      :on-click="addOneModule()"
+      v-on:click="addOneModule()"
     >
       Add
     </button>
-    <div v-if="addYes">
+    <div v-if="addModuleYes">
       <input
         type="text"
         id="create-module"
-        v-model="name"
+        v-model="moduleName"
         placeholder="Module Name"
-        class="appearance-none bg-transparent w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none border-b border-white placeholder:text-white"
+        class="appearance-none bg-transparent text-white mr-3 py-1 px-2 leading-tight focus:outline-none border-b border-white placeholder:text-white" :readonly="moduleEdit"
       />
       <input
         type="text"
         id="create-module"
-        v-model="name"
+        v-model="moduleDescription"
         placeholder="Module Description"
-        class="appearance-none bg-transparent w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none border-b border-white placeholder:text-white"
+        class="appearance-none bg-transparent text-white mr-3 w-4/5 py-1 px-2 leading-tight focus:outline-none border-b border-white placeholder:text-white" :readonly="moduleEdit"
       />
+      <button
+        class="m-5 h-16 bg-white/25 w-32 rounded-lg text-white font-semibold shadow-xl shadow-indigo-400/25 content-center p-2"
+        v-on:click="saveModule()"
+      >
+        Save
+      </button>
+      <button
+        class="m-5 h-16 bg-white/25 w-32 rounded-lg text-white font-semibold shadow-xl shadow-indigo-400/25 content-center p-2"
+        v-on:click="addLesson()"
+      >
+        Add a lesson
+      </button>
+      <div v-if="addLessonYes">
+        <input
+          type="text"
+          id="description"
+          v-model="description"
+          placeholder="Lesson Description"
+          class="appearance-none bg-transparent text-white w-2/6 m-3 py-1 px-2 leading-tight focus:outline-none border-b border-white placeholder:text-white"
+        />
+        <input
+          type="text"
+          id="question"
+          v-model="question"
+          placeholder="Question"
+          class="appearance-none bg-transparent text-white w-2/6 m-3 py-1 px-2 leading-tight focus:outline-none border-b border-white placeholder:text-white"
+        />
+        <input
+          type="text"
+          id="answer"
+          v-model="answer"
+          placeholder="Answer (T/F)"
+          class="appearance-none bg-transparent text-white m-3 py-1 px-2 leading-tight focus:outline-none border-b border-white placeholder:text-white"
+        />
+        <button v-on:click="saveLesson()" class="m-5 h-16 bg-white/25 w-32 rounded-lg text-white font-semibold shadow-xl shadow-indigo-400/25 content-center p-2">Save Lesson</button>
+      </div>
     </div>
     <p class="error" v-if="error">{{ error }}</p>
+    <div v-for="e in lessons" :key="e.description" class="text-white">
+      {{ e.description }}, {{e.question}}, {{e.answer}}
+    </div>
+    <button 
+      class="m-5 h-16 bg-white/25 w-32 rounded-lg text-white font-semibold shadow-xl shadow-indigo-400/25 content-center p-2"
+      v-on:click="saveModuleToBackend()">
+      Save Module!
+    </button>
     <button
       class="m-5 h-16 bg-white/25 w-32 rounded-lg text-white font-semibold shadow-xl shadow-indigo-400/25 content-center p-2"
       v-on:click="goBack()"
@@ -45,8 +89,11 @@ export default {
   data() {
     return {
       company: {},
-      modules: [],
-      addYes: false,
+      module: {},
+      lessons: [],
+      addModuleYes: false,
+      addLessonYes: false,
+      moduleEdit: false,
       error: "",
       text: "",
     };
@@ -59,8 +106,33 @@ export default {
       this.$router.back();
     },
     addOneModule() {
-      this.addYes = true;
+      this.addModuleYes = !this.addModuleYes;
     },
+    saveModule() {
+      this.module = {
+        name: this.moduleName,
+        description: this.moduleDescription,
+      }
+      this.moduleEdit = !this.moduleEdit
+    },
+    addLesson() {
+      this.addLessonYes = !this.addLessonYes;
+    },
+    saveLesson() {
+      this.lessons.push({
+        description: this.description,
+        question: this.question,
+        answer: this.answer
+      })
+      this.addLessonYes = !this.addLessonYes
+      this.description = ""
+      this.question = ""
+      this.answer = ""
+      console.log("lessons: ", this.lessons)
+    }, 
+    saveModuleToBackend() {
+      
+    }
   },
 };
 </script>
--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,10 @@
-const Sequelize = require("sequelize-cockroachdb");
+const Sequelize = require("sequelize");
 
 const databaseURL =
     "postgresql://ti:WD2ep8apXqYOG-PTBrbQoQ@ornery-ibex-4660.6wr.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full";
+
+//const databaseURL =
+//    "postgresql://mti:qGpbyTVrUG9rAz6DQlhCBQ@amazed-ostrich-6974.5xj.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full";
 
 const db = new Sequelize(databaseURL);
 
